b4ca47ca3a8f69657a297f63d89c80ce6f837287
xchen10bu <xchen@tibco.com>
2020-01-07 15:41:55 -0500

    commnet out tests

--- src/test/java/allbegray/slack/SlackRealTimeMessagingClientTest.java
+++ src/test/java/allbegray/slack/SlackRealTimeMessagingClientTest.java
@@ -1,32 +1,32 @@
-package allbegray.slack;
-
-import allbegray.slack.rtm.Event;
-import allbegray.slack.rtm.EventListener;
-import allbegray.slack.rtm.SlackRealTimeMessagingClient;
-import com.fasterxml.jackson.databind.JsonNode;
-
-public class SlackRealTimeMessagingClientTest {
-
-    private static String token = "your slack web api token";
-
-    public static void main(String args[]) {
-        SlackRealTimeMessagingClient realTimeMessagingClient = SlackClientFactory.createSlackRealTimeMessagingClient(token);
-        realTimeMessagingClient.addListener("hello", new HelloEventListener());
-        realTimeMessagingClient.addListener("message", new MessageEventListener());
-        realTimeMessagingClient.addListener(Event.MESSAGE, new EventListener() {
-            @Override
-            public void onMessage(JsonNode message) {
-                // todo
-            }
-        });
-        realTimeMessagingClient.connect();
-
-        try {
-            Thread.sleep(60 * 1000);
-            realTimeMessagingClient.close();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-}
+//package allbegray.slack;
+//
+//import allbegray.slack.rtm.Event;
+//import allbegray.slack.rtm.EventListener;
+//import allbegray.slack.rtm.SlackRealTimeMessagingClient;
+//import com.fasterxml.jackson.databind.JsonNode;
+//
+//public class SlackRealTimeMessagingClientTest {
+//
+//    private static String token = "your slack web api token";
+//
+//    public static void main(String args[]) {
+//        SlackRealTimeMessagingClient realTimeMessagingClient = SlackClientFactory.createSlackRealTimeMessagingClient(token);
+//        realTimeMessagingClient.addListener("hello", new HelloEventListener());
+//        realTimeMessagingClient.addListener("message", new MessageEventListener());
+//        realTimeMessagingClient.addListener(Event.MESSAGE, new EventListener() {
+//            @Override
+//            public void onMessage(JsonNode message) {
+//                // todo
+//            }
+//        });
+//        realTimeMessagingClient.connect();
+//
+//        try {
+//            Thread.sleep(60 * 1000);
+//            realTimeMessagingClient.close();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}
